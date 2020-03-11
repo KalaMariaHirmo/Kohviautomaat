0ef473683e773fda7c865d6113c92140445fca45
holger420 <hirmo.holger@gmail.com>
2020-03-11 16:42:16 +0200

    Tests

---
+++ Kohvimasin.Tests/Features/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Kohviautomaat.Controllers;
+using Kohviautomaat.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kohvimasin.Tests.Features
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestJoogidViewModel()
+		{
+			JoogidViewModel viewModel = new JoogidViewModel();
+			viewModel.id = 1;
+			viewModel.jooginimi = "test";
+			viewModel.topsepakis = 1337;
+
+			Assert.AreEqual(1, viewModel.id);
+			Assert.AreEqual("test", viewModel.jooginimi);
+			Assert.AreEqual(1337, viewModel.topsepakis);
+			Assert.AreEqual(0, viewModel.topsejuua);
+		}
+	}
+}
---
+++ Kohvimasin.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Kohvimasin.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Kohvimasin.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9c482709-89fd-4bac-b673-78a4199f2c27")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
